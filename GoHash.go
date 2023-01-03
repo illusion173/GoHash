2045aa18ea2ce251b5edb055962be314902e175e
Jeremiah Webb <illusion173@hotmail.com>
2023-01-02 22:08:48 -0500

    Began GoHash

--- GoHash.go
+++ GoHash.go
@@ -18,19 +18,30 @@ func main(){
   // Initially of course both slices will not have anything
   value_slice = make([]uint64, num_of_pairs, size_of_table)
   key_slice = make([]string, num_of_pairs, size_of_table)
+  insert(32, nil)
+}
+func insert(key any, value any) bool {
+  if key == nil{
+    panic("Key cannot be null")
+  }
+
+  if(value == nil){
+    fmt.Println("Warning value is nil with key", key)
+  }
+
+  return true
 }
 func hashCode(s string) uint64 {
         h := fnv.New64a()
         h.Write([]byte(s))
         return h.Sum64()
 }
-func hash(key interface{})(uint64) {
+func hash(key any)(uint64) {
 	if value, ok := key.(string); ok {
     var h uint64 = hashCode(value)
     fmt.Println(h)
 	}
-
-    var h uint64 = key.(uint64)
-    h ^= (h >> 20) ^ (h >> 12) ^ (h >> 7) & (h >> 4)
-  return 0
+  var h uint64 = key.(uint64)
+  h ^= (h >> 20) ^ (h >> 12) ^ (h >> 7) & (h >> 4)
+  return h
 }
