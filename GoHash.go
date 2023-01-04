05da507b424d9f5d707fc625e5bc89749116fc75
Jeremiah Webb <illusion173@hotmail.com>
2023-01-03 23:37:27 -0500

    Finished basic functionality, but only restricted to string keys and uint32 values

--- GoHash.go
+++ GoHash.go
@@ -18,17 +18,39 @@ var (
 // Begin here
 func main() {
 	fmt.Println("Beginning GoHash Program")
-	// unfortunate hard coding gotta learn generics for slices later
+	// Initial data to hash
+	keys_arr := [5]string{"John", "Jerry", "A.J", "Gabe", "Hannah"}
+	value_arr := [5]uint32{20, 21, 54, 23, 33}
 	// Initially of course both slices will not have anything
+	// These will operate as the arrays for hashed data
 	key_slice = make([]string, size_of_table)
 	value_slice = make([]uint32, size_of_table)
-	var test uint32 = 0
-	for i := 0; i <= 20; i++ {
-		put("test", test)
+	// Physical inputting of data for processing
+	for i := 0; i < len(keys_arr); i++ {
+		put(keys_arr[i], value_arr[i])
 	}
+
+	text, worked := get("John")
+	fmt.Println(text)
+	fmt.Println(worked)
 }
 
-// void
+func get(key any) (uint32, bool) {
+	if key == nil {
+		fmt.Println("Key cannot be nil in get operation")
+		return 0, false
+	}
+
+	i := hashKey(key)
+
+	for key_slice[i] != "" {
+		if key_slice[i] == key {
+			return value_slice[i], true
+		}
+	}
+	return 0, false
+}
+
 func put(key any, value any) {
 	if key == nil {
 		fmt.Println("Invalid Input")
@@ -45,15 +67,25 @@ func put(key any, value any) {
 	}
 
 	if num_of_pairs == (size_of_table / 2) {
-		// increase the size of the table prior
+		// Double table size if 50% full.
 		size_of_table *= 2
 		size_of_table_u *= 2
 		key_slice = resizeKey()
 		value_slice = resizeValue()
 	}
 
-	key_slice[0] = key.(string)
-	value_slice[0] = value.(uint32)
+	i := hashKey(key)
+
+	for key_slice[i] != "" {
+		if key_slice[i] == key {
+			value_slice[i] = value.(uint32)
+			return
+		}
+		i = ((i + 1) % size_of_table_u)
+	}
+
+	key_slice[i] = key.(string)
+	value_slice[i] = value.(uint32)
 	num_of_pairs += 1
 }
 
@@ -75,7 +107,7 @@ func hashCode(s string) uint32 {
 	return h.Sum32()
 }
 
-func hash(key any) uint32 {
+func hashKey(key any) uint32 {
 	var h uint32 = 0
 
 	switch key.(type) {
@@ -85,5 +117,5 @@ func hash(key any) uint32 {
 		h = key.(uint32)
 	}
 	h ^= (h >> 20) ^ (h >> 12) ^ (h >> 7) ^ (h >> 4)
-	return h & (size_of_table_u - 1)
+	return (h & (size_of_table_u - 1))
 }
