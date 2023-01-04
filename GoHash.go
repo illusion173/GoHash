34f45495992dce1bddfc0174c21ae9bb0f3d84a3
Jeremiah Webb <illusion173@hotmail.com>
2023-01-03 20:24:32 -0500

    Added resizing of arrays based on number of pairs

--- GoHash.go
+++ GoHash.go
@@ -9,9 +9,10 @@ var num_of_pairs int = 0
 
 // This is the initial size of the hashmap
 var (
-	size_of_table int = 10
-	key_slice     []string
-	value_slice   []uint64
+	size_of_table   int    = 10
+	size_of_table_u uint32 = 10
+	key_slice       []string
+	value_slice     []uint32
 )
 
 // Begin here
@@ -19,19 +20,53 @@ func main() {
 	fmt.Println("Beginning GoHash Program")
 	// unfortunate hard coding gotta learn generics for slices later
 	// Initially of course both slices will not have anything
-	key_slice = make([]string, num_of_pairs, size_of_table)
-	value_slice = make([]uint64, num_of_pairs, size_of_table)
+	key_slice = make([]string, size_of_table)
+	value_slice = make([]uint32, size_of_table)
+	var test uint32 = 0
+	for i := 0; i <= 20; i++ {
+		put("test", test)
+	}
 }
 
 // void
-func insert(key any, value any) {
+func put(key any, value any) {
 	if key == nil {
-		panic("Key cannot be null")
+		fmt.Println("Invalid Input")
+		fmt.Println("Key cannot be null")
+		// cancels insert
+		return
 	}
 
 	if value == nil {
+		fmt.Println("Invalid Input")
 		fmt.Println("Warning value is nil with key", key)
+		// cancels insert
+		return
 	}
+
+	if num_of_pairs == (size_of_table / 2) {
+		// increase the size of the table prior
+		size_of_table *= 2
+		size_of_table_u *= 2
+		key_slice = resizeKey()
+		value_slice = resizeValue()
+	}
+
+	key_slice[0] = key.(string)
+	value_slice[0] = value.(uint32)
+	num_of_pairs += 1
+}
+
+func resizeKey() []string {
+	newArr := make([]string, size_of_table)
+	copy(newArr, key_slice)
+	return newArr
+}
+
+func resizeValue() []uint32 {
+	newArr := make([]uint32, size_of_table)
+	copy(newArr, value_slice)
+	return newArr
 }
 
 func hashCode(s string) uint32 {
@@ -50,5 +85,5 @@ func hash(key any) uint32 {
 		h = key.(uint32)
 	}
 	h ^= (h >> 20) ^ (h >> 12) ^ (h >> 7) ^ (h >> 4)
-	return h & (uint32(size_of_table) - 1)
+	return h & (size_of_table_u - 1)
 }
